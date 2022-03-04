c0c540850082e20d0529c25bef1f1a406fc85e47
SmylerMC <smyler@mail.com>
2022-03-04 18:23:39 +0100

    Use draw callback & debug text

--- js/map.js
+++ js/map.js
@@ -2,18 +2,29 @@ class WorldMap {
 
 	#whenReady;
 	#shaderProgram;
+	#div;
 	#canvas;
+	#debugSpan;
 	#gl;
+	#lastFrameStartTime;
+	#lastFramesTimes = [];
 
-	constructor(canvas) {
-		this.canvas = canvas;
-		this.gl = canvas.getContext("experimental-webgl");
+	constructor(div) {
+		this.#div = div;
+		this.#div.classList.add("hodos-map");
+		this.#canvas = document.createElement("canvas");
+		this.#canvas.classList.add("hodos-canvas");
+		div.appendChild(this.#canvas);
+		this.#debugSpan = document.createElement("span");
+		this.#debugSpan.classList.add("hodos-debug");
+		div.appendChild(this.#debugSpan);
+		this.#gl = this.#canvas.getContext("experimental-webgl");
 	}
 
 	resize(width, height) {
-		this.canvas.width = width;
-		this.canvas.height = height;
-		this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+		this.#canvas.width = width;
+		this.#canvas.height = height;
+		this.#gl.viewport(0, 0, this.#canvas.width, this.#canvas.height);
 	}
 
 	async load() {
@@ -24,45 +35,49 @@ class WorldMap {
 	}
 
 	startRender() {
-		setInterval(() => this.#render(), 1000/10);
+		window.requestAnimationFrame(t => this.#render(t));
 	}
 
-	#render() {
-		this.gl.clearColor(0, 0.5, 0.8, 1);
-		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+	#render(timestamp) {
+		let start = new Date().getTime();
+		this.#gl.clearColor(0, 0.5, 0.8, 1);
+		this.#gl.clear(this.#gl.COLOR_BUFFER_BIT | this.#gl.DEPTH_BUFFER_BIT);
 		this.shaders.draw(this.tileTest);
+		let end = new Date().getTime();
+		let frameTime = end - start;
+		if (this.#lastFrameStartTime) {
+			this.#lastFramesTimes.push(start - this.#lastFrameStartTime);
+			if (this.#lastFramesTimes.length > 100) this.#lastFramesTimes.shift();
+		this.#debugSpan.innerText = "FPS: " + this.fps + " | Frame time: " + frameTime + "ms";
+		}
+		this.#lastFrameStartTime = start;
+		window.requestAnimationFrame(t => this.#render(t));
 	}
 
 	async #loadShaders() {
-		let vShader = new VertexShader(this.gl, "glsl/vertex.glsl");
-		let fShader = new FragmentShader(this.gl, "glsl/fragment.glsl");
+		let vShader = new VertexShader(this.#gl, "glsl/vertex.glsl");
+		let fShader = new FragmentShader(this.#gl, "glsl/fragment.glsl");
 		await Promise.all([
 			await vShader.load(),
 			await fShader.load()
 		]).then(() => {
-			let program = new ShaderProgram(this.gl, [vShader, fShader]);
+			let program = new ShaderProgram(this.#gl, [vShader, fShader]);
 			program.link();
 			program.use();
-			this.shaderProgram = program;
+			this.#shaderProgram = program;
 		}, () => console.log("Failed to load shaders"));
 	}
 
 	async #loadData() {
-		this.testTriangle1 = new Triangle(this.gl,
-			0, 0, 0,
-			0, 1, 0,
-			1, 1, 0
-		);
-		this.testTriangle2 = new Triangle(this.gl,
-			0, 0, 0,
-			0, -1, 0,
-			-1, -1, 0
-		);
-		this.tileTest = new BackedTile(this.gl);
+		this.tileTest = new BackedTile(this.#gl);
 	}
 
 	get shaders() {
-		return this.shaderProgram;
+		return this.#shaderProgram;
+	}
+
+	get fps() {
+		return Math.round(this.#lastFramesTimes.length  / this.#lastFramesTimes.reduce((sum, val) => sum + val) * 1000);
 	}
 
 }
