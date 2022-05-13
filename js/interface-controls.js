36b52b36168b0b9ec04bedfd91308dd1a5b889d1
Artamis <artamis@hackademint.org>
2022-05-13 19:05:27 +0200

    Color corrections. Modal creations and major layout work.

--- js/interface-controls.js
+++ js/interface-controls.js
@@ -14,13 +14,33 @@ document.getElementById("map").addEventListener('wheel', (e) => {
   e.preventDefault;
   console.log("Mouse");
   if (e.deltaY < 0) {
-    worldMap.controller.zoom(1);
+    worldMap.controller.zoom(0.4);
   } else {
-    worldMap.controller.zoom(-1);
+    worldMap.controller.zoom(-0.4);
   }
 })
 
 /*Toggle debug mode*/
 document.getElementById("debug-toggle").addEventListener('change', (e) => {
   worldMap.controller.toggleDebug();
+});
+
+/*Modal pop-up*/
+const toggleModal = (id) => {
+  let modal = document.getElementById(id);
+  if (modal.style.display == "none") {
+    $("#"+id).fadeIn(200);
+    modal.style.transform = "translateY(0px)";
+  } else {
+    $("#"+id).fadeOut(200);
+    modal.style.transform = "translateY(80px)";
+  }
+}
+
+document.getElementById("settings-modal-close").addEventListener('click', (e) => {
+  toggleModal("settings-modal");
+});
+
+document.getElementById("settings-modal-open").addEventListener('click', (e) => {
+  toggleModal("settings-modal");
 });
