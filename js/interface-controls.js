0bca2d123fc0f88715d4becd213a23a84a302091
Artamis <artamis@hackademint.org>
2022-05-13 20:09:19 +0200

    Corrections on modal layout. Responsive.

--- js/interface-controls.js
+++ js/interface-controls.js
@@ -26,21 +26,38 @@ document.getElementById("debug-toggle").addEventListener('change', (e) => {
 });
 
 /*Modal pop-up*/
+let openedModals = [];
+
 const toggleModal = (id) => {
   let modal = document.getElementById(id);
   if (modal.style.display == "none") {
+    //Check if other modal opened and close them
+    for (let i = openedModals.length - 1; i>=0; i--) {
+      toggleModal(openedModals.pop());
+    }
+    //Open requested modal
     $("#"+id).fadeIn(200);
     modal.style.transform = "translateY(0px)";
+    //Add it to opened modal list
+    openedModals.push(id)
   } else {
+    //Close requested modal
     $("#"+id).fadeOut(200);
     modal.style.transform = "translateY(80px)";
+    openedModals.splice(openedModals.indexOf(id))
   }
 }
 
-document.getElementById("settings-modal-close").addEventListener('click', (e) => {
-  toggleModal("settings-modal");
-});
+//Debug modal
+for (const element of document.getElementsByClassName("debug-modal-toggle")) {
+  element.addEventListener('click', (e) => {
+    toggleModal("debug-modal");
+  });
+}
 
-document.getElementById("settings-modal-open").addEventListener('click', (e) => {
-  toggleModal("settings-modal");
-});
+//Project modal
+for (const element of document.getElementsByClassName("project-modal-toggle")) {
+  element.addEventListener('click', (e) => {
+    toggleModal("project-modal");
+  });
+}
