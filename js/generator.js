4b5721bc6ae94bc93f2a9f4e383f275f808eba2b
Astate <astate085@gmail.com>
2022-05-21 01:42:25 +0200

    some var change in generation

--- js/generator.js
+++ js/generator.js
@@ -24,14 +24,14 @@ class MapGenerator {
     let time = Date.now();
     this.seedCells = Array();
     let trianglesVertices = getRandomPointsIn2dRange(
-      10000,
+      5000,
       0,
       WORLD_SIZE,
       this.#random
     );
     this.delaunay = d3.Delaunay.from(trianglesVertices);
     trianglesVertices = getRandomPointsIn2dRange(
-      10000,
+      5000,
       0,
       WORLD_SIZE,
       this.#random
@@ -41,8 +41,8 @@ class MapGenerator {
     this.cells = this.createAllCells(
       this.delaunay.voronoi([0, 0, WORLD_SIZE, WORLD_SIZE])
     );
-    this.generateMultipleContinentBurn(15, 0.1);
-    this.generateIsland(0.01, 0.4);
+    this.generateMultipleContinentBurn(25, 0.19);
+    this.generateIsland(0.005, 0.3);
     this.generateAltitude();
     this.generateBiome();
     this.generateCorruptedBurn();
